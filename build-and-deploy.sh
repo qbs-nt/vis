2ad9a09d28594d7309d78df8552f065ebe804519
Sebastian Busch <s.busch@obg-gruppe.de>
2019-03-13 14:46:00 +0100

    custom deployment script for portal

---
+++ build-and-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# choke on any error
+set -e
+
+# print each command (-v = before variable substitution, -x = after)
+# set -v
+set -x
+
+BUILD_DIR="./dist"
+DEPLOY_TARGET="/Volumes/local/obgweb2/htdocs/jscript/qbus-js/vis-develop--forked"
+
+if [ ! -d `dirname "${DEPLOY_TARGET}"` ]; then
+  echo "BASE DIR FOR DEPLOY TARGET '${DEPLOY_TARGET}' DOES NOT EXIST"
+  exit 1
+fi
+
+npm run build
+
+rm -rf "${DEPLOY_TARGET}"
+
+cp -R "${BUILD_DIR}" "${DEPLOY_TARGET}"
+
+obg-notifier.sh -title "timeline-plus Fork" -message "Built & deployed" -group visfork
+
+echo "Finished."
+
